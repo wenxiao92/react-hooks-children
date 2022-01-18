b5fa6f5c8c1b515bfe2cb80b177c40096e68c2ea
Wen Xiao <wennstyle@gmail.com>
2022-01-17 22:25:24 -0500

    commit tree 4abb428b683f5dc7e897a7986eb09f49486727ba

--- src/components/Container.js
+++ src/components/Container.js
@@ -6,7 +6,9 @@ function Container({
   textPosition = "", // possible values: left, right, center
   direction = "horizontal", // possible values: vertical, horizontal
   contentPosition = "center", // possible values: left, right, center
+  
 }) {
+  //console.log(children)
   return (
     <div className={`container ${textPosition}`}>
       {header ? <h2>{header}</h2> : null}
